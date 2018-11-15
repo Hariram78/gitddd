e5b024161750f3e8ecd63bfcb1de4518fee4576a
Deepak Verma <Deepak Verma@Stallion>
2018-11-15 08:47:04 +0530

    chander transferred to guwahati

--- gitd/src/main/java/com/understandinggit/git/ChanderClass.java
+++ gitd/src/main/java/com/understandinggit/git/ChanderClass.java
@@ -4,5 +4,6 @@ public class ChanderClass {
 
 	public static void main(String[] args) {
 		System.out.println("Chander lives in Haryana");
+		System.out.println("Chander changed to guwahati");
 	}
 }
